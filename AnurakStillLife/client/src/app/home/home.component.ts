4554d6200dbf4903236bd129383ba519fcf8eecd
anurak789 <anurak.net@gmail.com>
2024-07-19 16:31:23 +0700

    Added animation to home

--- AnurakStillLife/client/src/app/home/home.component.ts
+++ AnurakStillLife/client/src/app/home/home.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent {
 
