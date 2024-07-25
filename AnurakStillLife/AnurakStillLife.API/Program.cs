df1e483867cd7c4a6d772337c511cb3cc6360bd7
anurak789 <anurak.net@gmail.com>
2024-07-25 17:13:33 +0700

    add application insights

--- AnurakStillLife/AnurakStillLife.API/Program.cs
+++ AnurakStillLife/AnurakStillLife.API/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddApplicationInsightsTelemetry();
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
